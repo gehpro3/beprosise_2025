bb2ea60992c57b8c7d0def2ade4c383ec1f99480
Ginia Hawthorne-Prosise <chiefs_forth_0d@icloud.com>
2025-10-21 12:31:15 -0400

    Add icon and manifest for BP Blackjack Trainer

---
+++ app/src/main/java/app/run/us_west1/be_pro_sise_blackjack_dealer_trainer_v5_1022869262921/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.run.us_west1.be_pro_sise_blackjack_dealer_trainer_v5_1022869262921.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
